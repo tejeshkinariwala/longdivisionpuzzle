a65e0f4b0ef4dbab45a87d0c93a4fe30f4000199
tejeshkinariwala <56867177+tejeshkinariwala@users.noreply.github.com>
2020-06-29 09:31:24 +0800

    Update solve.only7.py
    updated the file where it prints rejection reasons

--- solve.only7.py
+++ solve.only7.py
@@ -15,12 +15,11 @@ def only7(num,place):
       return False
   return True 
 
-def longdiv(div, fac, D, Q, M, R):
-  #check constraints on div and fac
+def longdiv(div, fac, D, Q, M, R): 
   dl = len(str(div))
   fl = len(str(fac))
   D.append(int(str(div)[:len(str(fac))]))
-  for i in range(dl - fl + 1):
+  for i in range(dl - fl + 1): 
     Q.append(int(D[i]/fac))
     M.append(fac * Q[i])
     if len(M)==1:
@@ -28,15 +27,19 @@ def longdiv(div, fac, D, Q, M, R):
         return  
     if len(M)==2:
       if not no7(M[1]) or not isLen(M[1],7):
+        print "Mrej1", div,fac
         return  
     if len(M)==3:
       if not only7(M[2],5) or not isLen(M[2],6):
+        print "Mrej2", div,fac
         return  
     if len(M)==4:
       if not only7(M[3],3) or not isLen(M[3],7):
+        print "Mrej3", div,fac
         return  
     if len(M)==5:
       if not no7(M[4]) or not isLen(M[4],6):
+        print "Mrej4", div,fac
         return  
     R.append(D[i] - M[i])
     if i<dl-fl:
@@ -46,28 +49,30 @@ def longdiv(div, fac, D, Q, M, R):
         return
     if len(D)==3:
       if not only7(D[2],5) or not isLen(D[2],6):
+        print "Drej2", div,fac
         return
     if len(D)==4:
       if not no7(D[3]) or not isLen(D[3],7):
+        print "Drej3", div,fac
         return
     if len(D)==5:
       if not no7(D[4]) or not isLen(D[4],6):
+        print "Drej4", div,fac
         return
-  #check constraints
-  print "SOLUTION", div, fac	
+  print "SOLUTION", div, fac    
 
-thou = 0
+thou = 0 
 for fac in range(100070,999979):
   if only7(fac,2):
     if thou!=int(fac/1000):
       thou = int(fac/1000)
       print thou
-    temp = 9999999999/fac
-    for mul in range(10700,temp):
+    temp_min = 1000000000/fac
+    temp_max = 9999999999/fac
+    for mul in range(temp_min,temp_max):
       if not only7(mul,3):
         continue
-      div = fac * mul
+      div = fac * mul 
       if only7(div,8):
         D,Q,M,R = ([] for i in range(4))
-        longdiv(div,fac,D,Q,M,R)
 
